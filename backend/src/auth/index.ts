fcffba8b7b53043d09262eec656d9aef2b05bc4a
Mohfazam <mohd.sarwar.code@gmail.com>
2025-09-07 02:58:48 +0530

    SIgnUp Route SetUp

--- backend/src/auth/index.ts
+++ backend/src/auth/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
+const Prisma = new PrismaClient();
 
 export const authRouter = express.Router();
 
@@ -8,3 +10,37 @@ authRouter.get("/Health", (req, res) => {
         Msg: "Auth Route Up and Running"
     });
 });
+
+
+authRouter.post("/signUp", async (req, res) => {
+        const {name, email, password} = req.body;
+        try{
+            const existingUser = await Prisma.user.findUnique({
+                where: {email},
+            });
+
+            if(existingUser){
+                return res.status(400).json({
+                    Message: "Email already Registered"
+                });
+            }
+            const User = await Prisma.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: password
+            }
+        });
+
+         return res.status(200).json({
+            Message: "Sign Up SuccessFull",
+            User
+        });
+
+        } catch(err){
+            return res.status(500).json({
+                Msg: "Something Went Wrong",
+                error: err
+            });
+        }
+    });
