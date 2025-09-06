de056135803ad7fe68dd8d89dc05500a8d3679f7
Mohfazam <mohd.sarwar.code@gmail.com>
2025-09-06 19:47:52 +0530

    MINIMAL BACKEND

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hellow World");
+import express from "express";
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/Health", (req, res) => {
+    res.status(200).json({
+        Message: "Index Route Working Fine"
+    });
+});
+
+
+
+app.listen(4000, () => {
+    console.log("Server Started at port 4000");
+});
