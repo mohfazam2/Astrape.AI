5624743c411e51f1d6126b5fc24f585922ec72c7
Mohfazam <mohd.sarwar.code@gmail.com>
2025-09-08 16:25:04 +0530

    View All Products

--- frontend/src/components/AllProducts.tsx
+++ frontend/src/components/AllProducts.tsx
@@ -20,37 +20,34 @@ export const AllProducts = () => {
 
     useEffect(() => {
         const fetchProducts = async () => {
-          try {
-            setLoading(true);
-            const response = await axios.get<Product[]>(`${process.env.NEXT_PUBLIC_BASE_URL}/product/fetch`);
-            
-        
-            const productsData = response.data as Product[];
-            setProducts(productsData);
-            setError(null);
-          } catch (err) {
-            setError(err instanceof Error ? err.message : 'An error occurred');
-            console.error('Error fetching products:', err);
-          } finally {
-            setLoading(false);
-          }
+            try {
+                setLoading(true);
+                const response = await axios.get<Product[]>(`${process.env.NEXT_PUBLIC_BASE_URL}/product/fetch`);
+
+
+                const productsData = response.data as Product[];
+                setProducts(productsData);
+                setError(null);
+            } catch (err) {
+                setError(err instanceof Error ? err.message : 'An error occurred');
+                console.error('Error fetching products:', err);
+            } finally {
+                setLoading(false);
+            }
         };
-    
+
         fetchProducts();
-      }, []);
+    }, []);
 
     return (
         <div className="max-w-6xl mx-auto px-6">
             <div className="flex items-center gap-4">
                 <div className="bg-[#DB4444] w-5 h-10 rounded" />
-                <span className="text-[#DB4444] text-[18px]">Categories</span>
+                <span className="text-[#DB4444] text-[18px]">Our Products</span>
             </div>
 
             <div className="flex justify-between">
-                <h3 className="text-[48px] capitalize">Our Products</h3>
-                <button className="bg-[#DB4444] text-white h-14 w-38 rounded hover:bg-[#d65e5e] cursor-pointer">
-                    View All
-                </button>
+                <h3 className="text-[48px] capitalize">Explore Our Products</h3>
             </div>
 
             {loading ? (
@@ -64,15 +61,22 @@ export const AllProducts = () => {
             ) : products.length === 0 ? (
                 <div className="py-8 text-center">
                     {/* <p className="text-xl text-gray-500">No products found in {selectedCategory} category</p> */}
-                    <p className="text-gray-400 mt-2">Check back later for new items!</p>
+                    <p className="text-gray-400 mt-2">Check back later for new items! or you can add new products from the top nav bar</p>
                 </div>
             ) : (
                 <div className="py-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {products.map((product) => (
                         <ProductCard key={product.id} product={product} />
                     ))}
+                    
                 </div>
+                
             )}
+            <div className="w-full flex justify-center py-4">
+                        <button className="bg-[#DB4444] text-white h-14 w-38 rounded hover:bg-[#d65e5e] cursor-pointer">
+                            View All products
+                        </button>
+                    </div>
         </div>
     )
 }
